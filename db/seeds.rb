ffe1590659b948b53e82fe8287167b2a2f1172d5
Airat Zhanshuakov <dakanca@gmail.com>
2021-06-14 03:06:39 +0600

    feat: add course and lesson seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+4.times do
+  @course = Course.create(
+    title: Faker::Name.name,
+    description: Faker::Lorem.sentence(word_count: 3)
+  )
+  11.times do
+    @course.lessons.create(
+      title: Faker::Name.name,
+      description: Faker::Lorem.sentence(word_count: 3)
+    )
+  end
+end
